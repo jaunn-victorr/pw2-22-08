c82a07a4c7788e8dea42b74f41e463785ae401c9
jaunn-victorr <joao.vitor.ptal@gmail.com>
2022-09-12 17:11:35 +0000

    Genero.java

---
+++ app/src/main/java/pw2/repositories/GeneroRepository.java
@@ -0,0 +1,9 @@
+package pw2.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import exspring.models.Genero;
+
+public interface GeneroRepository extends CrudRepository<Genero, integer> {
+    
+}
