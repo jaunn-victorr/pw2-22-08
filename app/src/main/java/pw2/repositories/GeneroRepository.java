2c0ea1ebc72c3e1464759412f40f98b301e70c57
jaunn-victorr <joao.vitor.ptal@gmail.com>
2022-09-19 17:32:33 +0000

    Interger

--- app/src/main/java/pw2/repositories/GeneroRepository.java
+++ app/src/main/java/pw2/repositories/GeneroRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import pw2.models.Genero;
 
-public interface GeneroRepository extends CrudRepository<Genero, integer> {
+public interface GeneroRepository extends CrudRepository<Genero, Integer> {
     
 }
